d5cd0bf74c1c473908a81e5234f5f2919dc44f62
czl1378 <czl1378@126.com>
2025-02-08 00:47:46 +0800

    feat: too small funds limit

--- app/swap/swap-panel.tsx
+++ app/swap/swap-panel.tsx
@@ -150,6 +150,17 @@ export function SwapPanel() {
     window.history.replaceState(null, "", newUrl);
   };
 
+  const tooSmallFunds = useMemo(
+    () =>
+      coinA &&
+      new Decimal(
+        coinA.id === BITCOIN.id
+          ? formattedAmounts[Field.INPUT]
+          : formattedAmounts[Field.OUTPUT]
+      ).lt(0.0001),
+    [coinA, formattedAmounts]
+  );
+
   const handleSwitchCoins = () => {
     onSwitchCoins();
     const params = new URLSearchParams(searchParams?.toString() || "");
@@ -248,7 +259,8 @@ export function SwapPanel() {
                 swap.state === SwapState.INVALID ||
                 swap.state === SwapState.LOADING ||
                 insufficientBalance ||
-                !formattedAmounts[Field.OUTPUT]
+                !formattedAmounts[Field.OUTPUT] ||
+                tooSmallFunds
               }
             >
               {insufficientBalance
@@ -262,6 +274,8 @@ export function SwapPanel() {
                   ? "No Pool"
                   : swap.state === SwapState.INVALID
                   ? swap.errorMessage
+                  : tooSmallFunds
+                  ? "Too Small Funds"
                   : "Review"
                 : "Review"}
             </Button>
