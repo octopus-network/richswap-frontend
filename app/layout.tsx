885070da1d93a08db9cc8e90b6ec46ee6388d377
czl1378 <czl1378@126.com>
2025-02-08 09:55:28 +0800

    Ready to launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,38 @@ import { GeistSans } from "geist/font/sans";
 
 import "./globals.css";
 
+import { Providers } from "@/components/providers";
+import { Topbar } from "@/components/topbar";
+import { Footer } from "@/components/footer";
+import { ConnectWalletModal } from "@/components/connect-wallet-modal";
+import { Popups } from "@/components/popups";
+import { TransactionUpdater } from "@/components/transaction/updater";
+
 export const metadata: Metadata = {
   title: "RichSwap",
   description: "RichSwap",
 };
 
-export default function RootLayout({}: // children,
-Readonly<{
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${GeistSans.className} antialiased p-4`}>
-        Coming soon.
+      <body className={`${GeistSans.className} antialiased`}>
+        <Providers>
+          <div className="flex min-h-screen w-screen flex-col">
+            <Topbar />
+            <div className="flex flex-1 flex-col p-6 overflow-y-auto">
+              {children}
+            </div>
+            <Footer />
+            <Popups />
+          </div>
+          <ConnectWalletModal />
+          <TransactionUpdater />
+        </Providers>
       </body>
     </html>
   );
