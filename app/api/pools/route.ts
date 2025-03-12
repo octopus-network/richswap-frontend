16d57d8f984127429ef22a4c34e0be40ca25215c
czl1378 <czl1378@126.com>
2025-03-12 13:43:01 +0800

    fix: debug pool list

--- app/api/pools/route.ts
+++ app/api/pools/route.ts
@@ -1,15 +1,15 @@
 import { NextResponse } from "next/server";
-import axios from "axios";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-    const cache = await axios
-      .get(
-        "https://vquok3pr3bhc6tui.public.blob.vercel-storage.com/pool-list.json"
-      )
-      .then((res) => res.data);
+    const cache = await fetch(
+      "https://vquok3pr3bhc6tui.public.blob.vercel-storage.com/pool-list.json",
+      {
+        cache: "no-cache",
+      }
+    ).then((res) => res.json());
 
     return NextResponse.json({
       success: true,
