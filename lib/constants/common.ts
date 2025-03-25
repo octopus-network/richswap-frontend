85fdb44af7201ca605e5bf53f3f294e32d1c4820
czl1378 <czl1378@126.com>
2025-03-25 22:07:01 +0800

    fix: update icp host

--- lib/constants/common.ts
+++ lib/constants/common.ts
@@ -1,7 +1,7 @@
 import { NetworkType } from "@omnisat/lasereyes";
 
 export const UTXO_DUST = BigInt(546);
-export const ICP_HOST = "https://icp-api.io";
+export const ICP_HOST = "https://ic0.app";
 export const EXCHANGE_ID = "RICH_SWAP";
 
 export const NETWORK = (process.env.NEXT_PUBLIC_NETWORK ??
