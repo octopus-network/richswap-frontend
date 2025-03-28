04f9f9e6b74e885c7802232c0fc6f0ed939fdaef
czl1378 <czl1378@126.com>
2025-03-28 10:26:55 +0800

    fix: use isomorphic-fetch for icp actor

--- lib/exchange/actor.ts
+++ lib/exchange/actor.ts
@@ -2,13 +2,16 @@ import { Actor, HttpAgent } from "@dfinity/agent";
 import { ICP_HOST } from "../constants";
 import { idlFactory } from "../dids/richswap.did";
 
+import fetch from "isomorphic-fetch";
+
 const EXCHANGE_CANISTER_ID = process.env.NEXT_PUBLIC_EXCHANGE_CANISTER_ID!;
 
 export const actor = Actor.createActor(idlFactory, {
   agent: HttpAgent.createSync({
     host: ICP_HOST,
-    retryTimes: 30,
+    retryTimes: 50,
     verifyQuerySignatures: false,
+    fetch,
   }),
   canisterId: EXCHANGE_CANISTER_ID,
 });
