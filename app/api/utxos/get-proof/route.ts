62e7760ece2b2c9756be8e66936cea9491489d63
czl1378 <czl1378@126.com>
2025-06-10 12:55:16 +0800

    fix typo

--- app/api/utxos/get-proof/route.ts
+++ app/api/utxos/get-proof/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: NextRequest) {
       })
       .then((res) => res.data)
       .catch(() => ({
-        OK: [],
+        Ok: [],
       }));
 
     return NextResponse.json({
