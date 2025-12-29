9e745ec3b70898394d2a0e1d9d39b6ac152e63dd
czl1378 <czl1378@126.com>
2025-12-29 09:56:40 +0800

    feat: under maintanence

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
             check our
           </span>
           <Link
-            href="https://x.com/RichSwap_REE/status/1980091999610171893"
+            href="https://x.com/OmnityBTCdApps/status/2005457140488827252?s=20"
             target="_blank"
             className="text-foreground inline-flex text-lg ml-2"
           >
